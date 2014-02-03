e7fed2affcd3b56243051755cf8284a8dfcabc20
Kevin Jump <kevin@thejumps.co.uk>
2014-02-03 20:48:20 +0000

    v1.5.8.1 Special Edition
    Adds bulk API calls (to make ContentEdition work with it) and fixes
    aborts while waiting for an update)

--- jumps.umbraco.usync/Properties/AssemblyInfo.cs
+++ jumps.umbraco.usync/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("jumps.umbraco.usync")]
-[assembly: AssemblyDescription("umbraco syncing thing")]
+[assembly: AssemblyDescription("umbraco syncing thing - special no the api isn't working yet version")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("jumoo")]
 [assembly: AssemblyProduct("jumps.umbraco.usync")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyVersion("1.5.8.1")]
+[assembly: AssemblyFileVersion("1.5.8.1")]
