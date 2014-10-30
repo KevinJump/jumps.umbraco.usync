6d10bd81002eb42bca67fbefc62d3bba3972dfdf
Kevin Jump <kevin@thejumps.co.uk>
2014-10-30 10:19:21 +0000

    Revert "Version Bump (to v3)"
    This reverts commit 1efd019e4c9053913360915fb2b391a172829997.

--- jumps.umbraco.usync/Properties/AssemblyInfo.cs
+++ jumps.umbraco.usync/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("jumps.umbraco.usync")]
-[assembly: AssemblyDescription("An umbraco Syncing Thingy for Umbraco 6")]
+[assembly: AssemblyDescription("An umbraco Syncing Thingy for Umbraco 6.1.x")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("jumoo")]
 [assembly: AssemblyProduct("jumps.umbraco.usync")]
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.*")]
+[assembly: AssemblyVersion("1.6.*")]
