301ca5944bd1f44ce15c2e27df3a37ce4b9001b1
Kevin Jump <kevin@thejumps.co.uk>
2015-07-21 18:46:05 +0100

    Distribute update
    changes where the build actions go to (only works locally for me at the
    moment)

--- ToUmbracoInstall.cmd
+++ ToUmbracoInstall.cmd
@@ -12,7 +12,7 @@ REM and don't commit this file back.
 REM
 
 
-SET UmbracoPath=D:\Development\Web\Umbraco\DevUmbraco7.2.2
+SET UmbracoPath=D:\Development\Web\Umbraco\DevUmbraco7.2.6
 ECHO Copying File %1 to "%UmbracoPath%\%2"
 XCOPY %1 "%UmbracoPath%\%2" /y /i /q
 
